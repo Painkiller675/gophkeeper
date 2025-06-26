80f4bda6cd92b5a066607b0055a0863ddd26cf91
Painkiller675 <dimah1996@mail.ru>
2025-06-26 19:30:17 +0300

    corrections 1.5 local_storage + import fix

--- internal/client/storage/sqlite/sqllite.go
+++ internal/client/storage/sqlite/sqllite.go
@@ -10,15 +10,18 @@ import (
 	"github.com/Painkiller675/gophkeeper/internal/server/storage"
 )
 
+// localSecret is used to return data from local database
 type localSecret struct {
 	Content []byte
 	Version string
 }
 
+// LocalStorage represents a local client sqlite storage struct
 type LocalStorage struct {
 	LocalDB *sql.DB
 }
 
+// NewLocalStorage - a constructor of local sqlite database struct
 func NewLocalStorage() (*LocalStorage, error) {
 	lDB, err := initLocalDG()
 	if err != nil {
@@ -29,7 +32,7 @@ func NewLocalStorage() (*LocalStorage, error) {
 	}, nil
 }
 
-// initializeSQLite initializes the SQLite database
+// initLocalDG initializes the SQLite database
 func initLocalDG() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", "./local.db")
 	if err != nil {
@@ -52,6 +55,7 @@ func initLocalDG() (*sql.DB, error) {
 	return db, nil
 }
 
+// GetLocalSecret - returns a specific secret in offline mode
 func (ls *LocalStorage) GetLocalSecret(ctx context.Context, name string) (*localSecret, error) {
 	row := ls.LocalDB.QueryRowContext(
 		ctx,
@@ -66,6 +70,7 @@ func (ls *LocalStorage) GetLocalSecret(ctx context.Context, name string) (*local
 	return localSec, err
 }
 
+// SyncLocalSecrets - synchronize remote database with a local one
 func (ls *LocalStorage) SyncLocalSecrets(ctx context.Context, secret *proto.SecretInfo) error {
 	// Update local SQLite
 	_, err := ls.LocalDB.Exec("INSERT OR REPLACE INTO secrets (name, content, version) VALUES (?, ?, ?)",
